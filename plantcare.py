6dbb3508f333763332c3e7e0d1fb4b3d022bc427
Dmitry <dmi0@users.noreply.github.com>
2020-05-25 19:05:14 +0800

    Implement boundaries checking

--- plantcare.py
+++ plantcare.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+import json
 import logging
 import os
+import sys
 from time import sleep
 
 from plantsensor import PlantSensor, PlantSensorException
+
+DEFAULT_MAX_ATTEMPTS = 5
+DEFAULT_LOG_LEVEL = "INFO"
+DEFAULT_ADAPTER = "hci0"
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def read_sensors_config():
@@ -12,52 +20,94 @@ def read_sensors_config():
         raise ValueError("Environment variable SENSORS is not defined")
 
     result = {}
-    try:
-        for s in sensors_env.strip(" ;").split(";"):
-            (name, mac) = s.strip(" =").split("=")
-            result[name.strip()] = mac.strip()
-    except ValueError:
-        raise ValueError(("Environment variable SENSORS is expected in format: "
-                          "sensor1_name=sensor1_mac;sensor2_name=sensor2_mac, but '{}' is given"
-                          ).format(sensors_env))
+    for name, item in json.loads(sensors_env).items():
+        if "mac" not in item or "parameter" not in item:
+            raise ValueError("'mac' or 'parameter' is not found in {}".format(item))
+        val = {
+            "mac": item["mac"].strip(),
+            "parameter": item["parameter"].strip(),
+            "max": int(item["max"]) if "max" in item else sys.maxsize,
+            "min": int(item["min"]) if "min" in item else ~sys.maxsize,
+        }
+        if val["min"] > val["max"]:
+            raise ValueError("Wrong parameter boundaries: {}".format(item))
+        result[name.strip()] = val
     return result
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"),
-                        format='%(asctime)s [%(name)-24s] %(levelname)-8s %(message)s')
-    _LOGGER = logging.getLogger(__name__)
+def read_max_attempts_config():
+    if os.environ.get("MAX_ATTEMPTS") is None:
+        return DEFAULT_MAX_ATTEMPTS
+    try:
+        return int(os.environ.get("MAX_ATTEMPTS"))
+    except ValueError:
+        return DEFAULT_MAX_ATTEMPTS
 
-    sensors = {}
+
+def read_loglevel_config():
+    if os.environ.get("LOGLEVEL") is not None:
+        return os.environ.get("LOGLEVEL")
+    else:
+        return DEFAULT_LOG_LEVEL
+
+
+def read_adapter_config():
+    if os.environ.get("ADAPTER") is not None:
+        return os.environ.get("ADAPTER")
+    else:
+        return DEFAULT_ADAPTER
+
+
+def evaluate(name, parameter, min_value, max_value, values):
+    value = values[parameter]
+    if value < min_value or max_value < value:
+        _LOGGER.info(
+            "{}'s '{}'={} is out of the boundaries [{}, {}]".format(name, parameter, value, min_value, max_value)
+        )
+
+
+if __name__ == '__main__':
+    loglevel = read_loglevel_config()
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", loglevel),
+                        format='%(asctime)s [%(name)-24s] %(levelname)-8s %(message)s')
+    max_attempts = read_max_attempts_config()
+    adapter = read_adapter_config()
+
+    sensors = None
     try:
         sensors = read_sensors_config()
     except ValueError as e:
-        _LOGGER.error(e)
+        _LOGGER.exception("Failed to parse SENSORS configuration", exc_info=e)
         exit(1)
     _LOGGER.info("Configured sensors: {}".format(sensors))
 
-    sensors_to_process = dict((name, {"mac": mac, "attempts": 3}) for name, mac in sensors.items())
+    sensors_to_process = dict((name, max_attempts) for name in sensors)
+    success = 0
     while True:
         sensors_to_repeat = {}
-        for s_name, s_item in sensors_to_process.items():
+        for s_name, attempts in sensors_to_process.items():
+            s_item = sensors[s_name]
             try:
-                ps = PlantSensor(s_name, s_item["mac"])
+                ps = PlantSensor(s_name, s_item["mac"], adapter)
                 readings = ps.status()
                 _LOGGER.info("{} sensor readings: {}".format(s_name, readings))
+                evaluate(s_name, s_item["parameter"], s_item["min"], s_item["max"], readings)
+                success += 1
             except PlantSensorException as e:
-                attempts = sensors_to_process[s_name]["attempts"]
                 if attempts > 1:
                     attempts -= 1
                     _LOGGER.info(
                         "{} sensor reading failed, {}/{} attempt{} left ".format(
-                            s_name, attempts, 5,
+                            s_name, attempts, max_attempts,
                             "s" if attempts > 1 else "")
                     )
-                    sensors_to_repeat[s_name] = {"mac": s_item["mac"], "attempts": attempts}
+                    sensors_to_repeat[s_name] = attempts
                 else:
                     _LOGGER.error("{} sensor reading failed".format(s_name), exc_info=e)
         if len(sensors_to_repeat) > 0:
-            sleep(2)
+            sleep(5)
             sensors_to_process = sensors_to_repeat
         else:
             break
+
+    _LOGGER.info("Done. {}/{} are successfully processed".format(success, len(sensors)))
